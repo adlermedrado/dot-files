d15f1e70277ed0bc57d99e67a9fbc4eec8f85735
Adler Medrado <adlermedrado@gmail.com>
2018-09-23 21:51:21 -0300

    Move common terminal config to include file

--- bash/include.sh
+++ bash/include.sh
@@ -1,5 +1,10 @@
+export PATH="/home/adler/.pyenv/bin:$PATH"
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
 c() { 
     cd ~/devel/$* && pipenv shell;
 }
-alias ct='ctags -R --verbose=yes .'
 
+alias ct='ctags -R --verbose=yes .'
