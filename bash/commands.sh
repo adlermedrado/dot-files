ac2b8e6648ac7119e9b3aef4bd4383de5af47662
Adler Medrado <adlermedrado@gmail.com>
2019-03-25 12:09:05 -0300

    Add aliases

--- bash/commands.sh
+++ bash/commands.sh
@@ -9,3 +9,12 @@ dp() {
 ctp() {
     ctags -V -R --fields=+l --languages=python --python-kinds=-iv -f ./tags ./ $(python -c "import os, sys; print(' '.join('{}'.format(d) for d in sys.path if os.path.isdir(d)))")
 }
+
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias grep='grep --color=auto'
+alias l='ls -CF'
+alias la='ls -A'
+alias ll='ls -alF'
+alias ls='ls --color=auto'
