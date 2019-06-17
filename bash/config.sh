fbf2e84bc8664dcab430a74b6f53f9051843a110
Adler Medrado <adlermedrado@gmail.com>
2019-06-16 21:17:36 -0300

    Config bashblog

--- bash/config.sh
+++ bash/config.sh
@@ -1,6 +1,7 @@
 # update Path and prompt
 PATH="$HOME/.local/bin:$HOME/bin:$PATH"
 PATH="$HOME/.pyenv/bin:/home/amedrado/.local/bin:$PATH"
+PATH="$HOME/devel/bashblog:$PATH"
 export PATH
 
 PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]"
@@ -11,3 +12,4 @@ export PS1
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
+export EDITOR=vim
