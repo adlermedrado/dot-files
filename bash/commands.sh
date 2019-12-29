438a4f2a37bd5f41a5aff3c3af51efc1fd5354dd
Adler Medrado <adlermedrado@gmail.com>
2019-12-29 16:44:15 -0300

    Remove pyenv and other stuff

--- bash/commands.sh
+++ bash/commands.sh
@@ -1,11 +1,3 @@
-c() { 
-    cd ~/devel/$* && pyenv activate $*;
-}
-
-dp() {
-    pyenv deactivate
-}
-
 ctp() {
     ctags -V -R --fields=+l --languages=python --python-kinds=-iv -f ./tags ./ $(python -c "import os, sys; print(' '.join('{}'.format(d) for d in sys.path if os.path.isdir(d)))")
 }
--- bash/config.sh
+++ bash/config.sh
@@ -8,8 +8,4 @@ PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]"
 PS1=$PS1"(\$(git branch 2>/dev/null | grep '^*' | colrm 1 2)) \$ "
 export PS1
 
-# Pyenv Config
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-
 export EDITOR=vim
