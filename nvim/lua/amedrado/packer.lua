71c324abcb535d8e1c9813d55927ae400992999c
Adler Medrado <adler@adlermedrado.com.br>
2023-09-16 11:18:06 -0300

    Fix typo

--- nvim/lua/amedrado/packer.lua
+++ nvim/lua/amedrado/packer.lua
@@ -1,4 +1,4 @@
-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
 vim.cmd.packadd('packer.nvim')
