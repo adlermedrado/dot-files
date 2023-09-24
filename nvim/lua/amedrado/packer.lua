33008d6c2eff7e045ba4570cf60f8b1aea88fe1c
Adler Medrado <adler@adlermedrado.com.br>
2023-09-24 09:34:02 -0300

    Update dependencies

--- nvim/lua/amedrado/packer.lua
+++ nvim/lua/amedrado/packer.lua
@@ -9,7 +9,6 @@ return require('packer').startup(function(use)
 
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        -- or                            , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
