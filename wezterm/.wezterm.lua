8365bcfd325cc64501ac3adefa41ff5520cc6681
Adler Medrado <adler@adlermedrado.com.br>
2023-10-06 16:09:18 -0300

    Remove comments

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,25 +1,14 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
--- This table will hold the configuration.
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
-
--- config.color_scheme = 'FarSide (terminal.sexy)'
 config.color_scheme = 'rose-pine-dawn'
 
 config.font = wezterm.font('JetBrains Mono', { weight = 'Bold', italic = true })
-
 config.font_size = 15.0
 
--- and finally, return the configuration to wezterm
 return config
