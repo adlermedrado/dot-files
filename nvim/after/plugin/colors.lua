83e45566789bafbaeae90edf1429aba12049e19c
Adler Medrado <adler@adlermedrado.com.br>
2023-11-27 18:58:28 -0300

    Change vim colors to Tokyo Night theme

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,10 +1,9 @@
-require('rose-pine').setup({
-	disable_background = true,
-    dark_variant = 'moon'
+require('tokyonight').setup({
+	disable_background = true
 })
 
 function ColorMyPencils(color)
-	color = color or "rose-pine"
+	color = color or "tokyonight-night"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
