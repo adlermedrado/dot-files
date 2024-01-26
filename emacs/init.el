137192e76994a4bdb620c2aa94360acf0e6f2f5a
Adler Medrado <adler@adlermedrado.com.br>
2024-01-25 22:47:26 -0300

    use symlink to init.el and tokyo-night theme

--- emacs/init.el
+++ emacs/init.el
@@ -120,7 +120,7 @@ NAME and ARGS are as in `use-package'."
   ;; Enable custom neotree theme (all-the-icons must be installed!)
   (doom-themes-neotree-config)
   ;; or for treemacs users
-  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (setq doom-themes-treemacs-theme "doom-tokyo-night") ; use "doom-colors" for less minimal icon theme
   ;; (doom-themes-treemacs-config)
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
@@ -132,3 +132,4 @@ NAME and ARGS are as in `use-package'."
 
 ;; start the initial frame maximized
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
+(setq vc-follow-symlinks t)
